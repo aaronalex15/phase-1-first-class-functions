4cdfe37180a1d571897e36a36f1321c284566657
Aaron Johnson <aaronjohnson@Pauls-MBP.lan>
2024-01-30 14:55:49 -0800

    first class functions

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function receivesAFunction(callback) {
+    callback();
+}
+function returnsANamedFunction() {
+    return returnsANamedFunction
+}
+
+
+let returnsAnAnonymousFunction = function() {
+    return function() {
+    }
+} 
+
